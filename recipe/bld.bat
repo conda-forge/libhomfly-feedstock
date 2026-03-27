2a0983d0fe017ae74b7cb99391f745dbb9f554a7
cxzhong <chenxin.zhong@outlook.com>
2026-03-27 15:47:29 +0800

    Use MSVC since ibdw-gc use MSVC to compile

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,33 @@
-echo source %SYS_PREFIX:\=/%/etc/profile.d/conda.sh    > conda_build.sh
-echo conda activate "${PREFIX}"                       >> conda_build.sh
-echo conda activate --stack "${BUILD_PREFIX}"         >> conda_build.sh
-echo CONDA_PREFIX=${CONDA_PREFIX//\\//}               >> conda_build.sh
-type "%RECIPE_DIR%\build.sh"                          >> conda_build.sh
+@echo off
 
-set PREFIX=%PREFIX:\=/%
-set BUILD_PREFIX=%BUILD_PREFIX:\=/%
-set CONDA_PREFIX=%CONDA_PREFIX:\=/%
-set SRC_DIR=%SRC_DIR:\=/%
-set MSYSTEM=UCRT64
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
-bash -lc "./conda_build.sh"
-if errorlevel 1 exit 1
+cd lib
+
+cl /nologo /O2 /MD /I. /I"%LIBRARY_INC%" /c ^
+	bound.c control.c dllink.c homfly.c knot.c model.c order.c poly.c
+if errorlevel 1 exit /b 1
+
+link /nologo /DLL /OUT:homfly.dll /IMPLIB:homfly.lib ^
+	bound.obj control.obj dllink.obj homfly.obj knot.obj model.obj order.obj poly.obj ^
+	"%LIBRARY_LIB%\gc.lib"
+if errorlevel 1 exit /b 1
+
+cd ..\test
+cl /nologo /O2 /MD /I. /I..\lib /I"%LIBRARY_INC%" /c test_example.c
+if errorlevel 1 exit /b 1
+
+link /nologo /OUT:test_example.exe test_example.obj ..\lib\homfly.lib "%LIBRARY_LIB%\gc.lib"
+if errorlevel 1 exit /b 1
+
+set "PATH=%CD%\..\lib;%PATH%"
+test_example.exe data.txt
+if errorlevel 1 exit /b 1
+
+cd ..\lib
+copy homfly.dll "%LIBRARY_BIN%\"
+if errorlevel 1 exit /b 1
+
+copy homfly.lib "%LIBRARY_LIB%\"
+if errorlevel 1 exit /b 1
+
+copy homfly.h "%LIBRARY_INC%\"
+if errorlevel 1 exit /b 1
